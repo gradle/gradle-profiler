16d630626d6f7c0f3d9bcc404c3f860ef62b8b23
Adam Murdoch <adam@gradle.com>
2016-08-22 11:29:05 +1000

    Capture output in a log file.

--- src/main/java/net/rubygrapefruit/gradle/profiler/Main.java
+++ src/main/java/net/rubygrapefruit/gradle/profiler/Main.java
@@ -10,10 +10,9 @@ import org.gradle.tooling.GradleConnector;
 import org.gradle.tooling.ProjectConnection;
 import org.gradle.tooling.model.build.BuildEnvironment;
 
-import java.io.ByteArrayOutputStream;
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Main {
@@ -22,10 +21,11 @@ public class Main {
         try {
             ok = run(args);
         } catch (Exception e) {
-            e.printStackTrace();
+            e.printStackTrace(System.out);
             ok = false;
         }
         System.out.println();
+        System.out.flush();
         System.exit(ok ? 0 : 1);
     }
 
@@ -47,6 +47,9 @@ public class Main {
             parser.printHelpOn(System.out);
             return false;
         }
+
+        setupLogging();
+
         File projectDir = (parsedOptions.has(projectOption) ? new File(parsedOptions.valueOf(projectOption)) : new File(".")).getCanonicalFile();
         System.out.println();
         System.out.println("Project dir: " + projectDir);
@@ -91,6 +94,12 @@ public class Main {
         return true;
     }
 
+    private static void setupLogging() throws FileNotFoundException {
+        File logFile = new File("profile.log");
+        OutputStream log = new BufferedOutputStream(new FileOutputStream(logFile));
+        System.setOut(new PrintStream(new TeeOutputStream(System.out, log)));
+    }
+
     private static void startOperation(String name) {
         System.out.println();
         System.out.println("* " + name);
@@ -121,5 +130,51 @@ public class Main {
             throw e;
         }
         System.out.println("Used daemon with pid " + pidInstrumentation.getPidForLastBuild());
+    }
+
+    static class TeeOutputStream extends OutputStream {
+        private final List<OutputStream> targets;
+
+        public TeeOutputStream(OutputStream... targets) {
+            this.targets = Arrays.asList(targets);
+        }
+
+        @Override
+        public void write(int b) throws IOException {
+            withAll(stream -> stream.write(b));
+        }
+
+        @Override
+        public void write(byte[] b, int off, int len) throws IOException {
+            withAll(stream -> stream.write(b, off, len));
+        }
+
+        @Override
+        public void flush() throws IOException {
+            withAll(stream -> stream.flush());
+        }
+
+        @Override
+        public void close() throws IOException {
+            withAll(stream -> stream.close());
+        }
+
+        private void withAll(IOAction action) throws IOException {
+            IOException failure = null;
+            for (OutputStream target : targets) {
+                try {
+                    action.withStream(target);
+                } catch (IOException e) {
+                    failure = e;
+                }
+            }
+            if (failure != null) {
+                throw failure;
+            }
+        }
+
+        private interface IOAction {
+            void withStream(OutputStream stream) throws IOException;
+        }
     }
 }
