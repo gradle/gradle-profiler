0b2e9e63069625f3fe3d859604b9c4ba3e3d8699
Cedric Champeau <cedric.champeau@gmail.com>
2016-12-13 14:13:52 +0100

    Fix accidental quote in profiler args

--- src/main/java/org/gradle/profiler/hp/HonestProfilerJvmArgsCalculator.java
+++ src/main/java/org/gradle/profiler/hp/HonestProfilerJvmArgsCalculator.java
@@ -33,7 +33,7 @@ public class HonestProfilerJvmArgsCalculator extends JvmArgsCalculator {
         jvmArgs.add("-XX:+DebugNonSafepoints");
         jvmArgs.add("-agentpath:" + args.getHpHomeDir().getAbsolutePath() + "/liblagent.so=interval=" + args.getInterval()
                 + ",logPath=" + args.getLogPath().getAbsolutePath()
-                + ",port=" + args.getPort() + ",host=localhost,start=0'"
+                + ",port=" + args.getPort() + ",host=localhost,start=0"
                 + ",maxFrames=" + args.getMaxFrames());
     }
 }
