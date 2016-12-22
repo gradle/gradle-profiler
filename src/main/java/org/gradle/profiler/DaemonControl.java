632f8597698386fab820725140933af5ba712292
Ingo Kegel <ingo.kegel@ej-technologies.com>
2016-12-22 17:46:54 +0100

    Add support for running on Windows

--- src/main/java/org/gradle/profiler/DaemonControl.java
+++ src/main/java/org/gradle/profiler/DaemonControl.java
@@ -1,9 +1,7 @@
 package org.gradle.profiler;
 
-import java.io.File;
-
 public class DaemonControl {
     public void stop(GradleVersion version) {
-        new CommandExec().run(new File(version.getGradleHome(), "bin/gradle").getAbsolutePath(), "--stop");
+        version.runGradle("--stop");
     }
 }
