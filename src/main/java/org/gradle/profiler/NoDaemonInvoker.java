5e18d19ff2acca903481fd3d9a7ded47461a8136
Stefan Oehme <stefan@gradle.com>
2017-04-01 23:06:18 +0200

    Fix --no-daemon option

--- src/main/java/org/gradle/profiler/NoDaemonInvoker.java
+++ src/main/java/org/gradle/profiler/NoDaemonInvoker.java
@@ -8,13 +8,14 @@ import java.util.function.Consumer;
 public class NoDaemonInvoker extends CliInvoker {
 
     public NoDaemonInvoker(GradleVersion gradleVersion, File javaHome, File projectDir, List<String> jvmArgs, List<String> gradleArgs, PidInstrumentation pidInstrumentation, Consumer<BuildInvocationResult> resultsConsumer) {
-        super(gradleVersion, javaHome, projectDir, jvmArgs, withNoDaemonOption(gradleArgs), pidInstrumentation, resultsConsumer);
+        super(gradleVersion, javaHome, projectDir, jvmArgs, gradleArgs, pidInstrumentation, resultsConsumer);
     }
 
-    private static List<String> withNoDaemonOption(List<String> gradleArgs) {
-        List<String> args = new ArrayList<>();
-        args.addAll(gradleArgs);
-        args.add("--no-daemon");
-        return args;
+    @Override
+    protected void run(List<String> tasks, List<String> gradleArgs, List<String> jvmArgs) {
+        List<String> withNoDaemon = new ArrayList<>();
+        withNoDaemon.addAll(gradleArgs);
+        withNoDaemon.add("--no-daemon");
+        super.run(tasks, withNoDaemon, jvmArgs);
     }
 }
