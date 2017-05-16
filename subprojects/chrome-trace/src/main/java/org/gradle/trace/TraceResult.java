7fcb59bce0eefeb7719b1d9c1d340ebca1eeb671
Jendrik Johannes <jendrik@gradle.com>
2017-05-16 16:56:03 +0200

    Spool events directly to disc using a temp file

--- subprojects/chrome-trace/src/main/java/org/gradle/trace/TraceResult.java
+++ subprojects/chrome-trace/src/main/java/org/gradle/trace/TraceResult.java
@@ -1,25 +1,25 @@
 package org.gradle.trace;
 
+import org.gradle.api.invocation.Gradle;
 import org.gradle.internal.UncheckedException;
 
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.PrintWriter;
+import java.io.*;
+import java.nio.file.Files;
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
 public class TraceResult {
-    private final Map<String, TraceEvent> events = new LinkedHashMap<>();
+    private final Map<String, DurationEvent> events = new LinkedHashMap<>();
+    private File tempTraceFile;
+    private PrintWriter writer;
 
     public void count(String name, String metric, Map<String, Double> info) {
         count(name, metric, info, null);
     }
 
     public void count(String name, String metric, Map<String, Double> info, String colorName) {
-        events.put(name, new CountEvent(metric, info, colorName));
+        streamToTraceFile(new CountEvent(metric, info, colorName));
     }
 
     public void start(String name, String category, long timestampNanos) {
@@ -31,38 +31,92 @@ public class TraceResult {
     }
 
     public void finish(String name, long timestampNanos, Map<String, String> info) {
-        DurationEvent event = (DurationEvent) events.get(name);
+        DurationEvent event = events.remove(name);
         if (event != null) {
             event.finished(timestampNanos);
             event.getInfo().putAll(info);
+            streamToTraceFile(event);
         }
     }
 
-    public void writeEvents(File traceFile) {
-        PrintWriter writer = getPrintWriter(traceFile);
+    private void streamToTraceFile(TraceEvent event) {
+        if (writer == null) {
+            startTraceFile();
+        } else {
+            writer.print(',');
+        }
+        writer.print(event.toString());
+    }
+
+    private void startTraceFile() {
+        try {
+            tempTraceFile = Files.createTempFile("gradle-trace", ".html").toFile();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        copyResourceToTraceFile("/trace-header.html", false);
+        writer = createPrintWriter();
         writer.println("{\n" +
                 "  \"traceEvents\": [\n");
-
-        Iterator<TraceEvent> itr = events.values().iterator();
-        while (itr.hasNext()) {
-            writer.print(itr.next().toString());
-            writer.println(itr.hasNext() ? "," : "");
-        }
-
-        writer.println("],\n" +
-                "  \"displayTimeUnit\": \"ns\",\n" +
-                "  \"systemTraceEvents\": \"SystemTraceData\",\n" +
-                "  \"otherData\": {\n" +
-                "    \"version\": \"My Application v1.0\"\n" +
-                "  }\n" +
-                "}\n");
     }
 
-    private PrintWriter getPrintWriter(File jsonFile) {
+    public void finalizeTraceFile(Gradle gradle) {
+        for (DurationEvent unfinishedEvent : events.values()) {
+            streamToTraceFile(unfinishedEvent);
+        }
+
+        if (tempTraceFile != null) {
+            writer.println("],\n" +
+                    "  \"displayTimeUnit\": \"ns\",\n" +
+                    "  \"systemTraceEvents\": \"SystemTraceData\",\n" +
+                    "  \"otherData\": {\n" +
+                    "    \"version\": \"My Application v1.0\"\n" +
+                    "  }\n" +
+                    "}\n");
+            writer.close();
+
+            copyResourceToTraceFile("/trace-footer.html", true);
+
+            File finalTraceFile = traceFile(gradle);
+            tempTraceFile.renameTo(finalTraceFile);
+            gradle.getRootProject().getLogger().lifecycle("Trace written to file://" + finalTraceFile.getAbsolutePath());
+
+        }
+    }
+
+    private PrintWriter createPrintWriter() {
         try {
-            return new PrintWriter(new FileWriter(jsonFile, true), true);
+            return new PrintWriter(new FileWriter(tempTraceFile, true), true);
         } catch (IOException e) {
             throw UncheckedException.throwAsUncheckedException(e);
         }
     }
+
+    private void copyResourceToTraceFile(String resourcePath, boolean append) {
+        try (OutputStream out = new FileOutputStream(tempTraceFile, append);
+             InputStream in = getClass().getResourceAsStream(resourcePath)) {
+            byte[] buffer = new byte[1024];
+            int len;
+            while ((len = in.read(buffer)) != -1) {
+                out.write(buffer, 0, len);
+            }
+        } catch (IOException e) {
+            throw UncheckedException.throwAsUncheckedException(e);
+        }
+    }
+
+    private File traceFile(Gradle gradle) {
+        File traceFile = (File) gradle.getRootProject().findProperty("chromeTraceFile");
+        if (traceFile == null) {
+            traceFile = defaultTraceFile(gradle);
+        }
+        traceFile.getParentFile().mkdirs();
+        return traceFile;
+    }
+
+    private File defaultTraceFile(Gradle gradle) {
+        File buildDir = gradle.getRootProject().getBuildDir();
+        return new File(buildDir, "trace/task-trace.html");
+    }
+
 }
