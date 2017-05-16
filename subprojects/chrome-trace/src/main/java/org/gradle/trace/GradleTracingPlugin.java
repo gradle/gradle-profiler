12ebb57175fb2f560d0c86cbdc08f073cb3cf04a
Jendrik Johannes <jendrik@gradle.com>
2017-05-16 17:50:16 +0200

    Separate classes for different monitoring concerns

--- subprojects/chrome-trace/src/main/java/org/gradle/trace/GradleTracingPlugin.java
+++ subprojects/chrome-trace/src/main/java/org/gradle/trace/GradleTracingPlugin.java
@@ -1,155 +1,59 @@
 package org.gradle.trace;
 
-import com.sun.management.GarbageCollectionNotificationInfo;
-import com.sun.management.OperatingSystemMXBean;
 import org.gradle.BuildAdapter;
 import org.gradle.BuildResult;
 import org.gradle.api.Plugin;
 import org.gradle.api.invocation.Gradle;
 import org.gradle.initialization.BuildRequestMetaData;
 import org.gradle.trace.listener.BuildOperationListenerAdapter;
+import org.gradle.trace.monitoring.GCMonitoring;
+import org.gradle.trace.monitoring.SystemMonitoring;
+import org.gradle.trace.util.TimeUtil;
 
 import javax.inject.Inject;
-import javax.management.ListenerNotFoundException;
-import javax.management.NotificationEmitter;
-import javax.management.NotificationListener;
-import javax.management.openmbean.CompositeData;
-import java.lang.management.GarbageCollectorMXBean;
-import java.lang.management.ManagementFactory;
-import java.lang.management.MemoryUsage;
 import java.util.HashMap;
-import java.util.LinkedHashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
 
 public class GradleTracingPlugin implements Plugin<Gradle> {
     private static final String CATEGORY_PHASE = "BUILD_PHASE";
     private static final String PHASE_BUILD = "build duration";
-    private static final String GARBAGE_COLLECTION = "GARBAGE_COLLECTION";
+
     private final BuildRequestMetaData buildRequestMetaData;
     private final TraceResult traceResult = new TraceResult();
-    private final OperatingSystemMXBean operatingSystemMXBean;
-    private final List<GarbageCollectorMXBean> garbageCollectorMXBeans;
-    private int sysPollCount = 0;
-    private final long jvmStartTime;
-    private Map<GarbageCollectorMXBean, NotificationListener> gcNotificationListeners = new HashMap<>();
-    private ScheduledExecutorService scheduledExecutorService = Executors.newSingleThreadScheduledExecutor();
-    private final long maxHeap;
+    private final SystemMonitoring systemMonitoring = new SystemMonitoring();
+    private final GCMonitoring gcMonitoring = new GCMonitoring();
     private BuildOperationListenerAdapter buildOperationListener;
 
     @Inject
     public GradleTracingPlugin(BuildRequestMetaData buildRequestMetaData) {
         this.buildRequestMetaData = buildRequestMetaData;
-        this.operatingSystemMXBean = (OperatingSystemMXBean) ManagementFactory.getOperatingSystemMXBean();
-        this.garbageCollectorMXBeans = ManagementFactory.getGarbageCollectorMXBeans();
-        this.jvmStartTime = ManagementFactory.getRuntimeMXBean().getStartTime();
-        this.maxHeap = Runtime.getRuntime().maxMemory();
     }
 
     @Override
     public void apply(Gradle gradle) {
-        scheduledExecutorService.scheduleAtFixedRate(() -> {
-            HashMap<String, Double> cpuStats = new HashMap<>();
-            double pcpu = operatingSystemMXBean.getProcessCpuLoad() * 100;
-            double scpu = operatingSystemMXBean.getSystemCpuLoad() * 100;
-
-            if (!Double.isNaN(pcpu) && !Double.isNaN(scpu)) {
-                cpuStats.put("process_cpu_used", pcpu);
-                cpuStats.put("non_process_cpu_used", scpu - pcpu);
-
-                traceResult.count("cpu" + sysPollCount, "cpu", cpuStats);
-            }
-
-            sysPollCount++;
-        }, 0, 500, TimeUnit.MILLISECONDS);
-
-        for (GarbageCollectorMXBean garbageCollectorMXBean : garbageCollectorMXBeans) {
-            NotificationEmitter emitter = (NotificationEmitter) garbageCollectorMXBean;
-            NotificationListener gcNotificationListener = (notification, handback) -> {
-                if (notification.getType().equals(GarbageCollectionNotificationInfo.GARBAGE_COLLECTION_NOTIFICATION)) {
-                    GarbageCollectionNotificationInfo info = GarbageCollectionNotificationInfo.from((CompositeData) notification.getUserData());
-                    //get all the info and pretty print it
-                    long duration = info.getGcInfo().getDuration();
-                    String gctype = info.getGcAction();
-                    Map<String, String> args = new HashMap<>();
-                    args.put("type", gctype);
-
-                    String colorName = "good";
-                    if (gctype.equals("end of major GC")) {
-                        colorName = "terrible";
-                    }
-
-                    traceResult.start("GC" + info.getGcInfo().getId(), GARBAGE_COLLECTION, toNanoTime(jvmStartTime + info.getGcInfo().getStartTime()), colorName);
-                    traceResult.finish("GC" + info.getGcInfo().getId(), toNanoTime(jvmStartTime + info.getGcInfo().getEndTime()), args);
-                    Map<String, MemoryUsage> pools = info.getGcInfo().getMemoryUsageAfterGc();
-                    long unallocatedHeap = maxHeap;
-                    HashMap<String, Double> gcInfo = new LinkedHashMap<>();
-                    for (String pool : pools.keySet()) {
-                        MemoryUsage usage = pools.get(pool);
-                        gcInfo.put(pool, (double) usage.getUsed());
-                        unallocatedHeap -= usage.getUsed();
-                    }
-                    gcInfo.put("unallocated", (double) unallocatedHeap);
-
-                    traceResult.count("heap" + info.getGcInfo().getId(), "heap", gcInfo);
-                }
-            };
-            emitter.addNotificationListener(gcNotificationListener, null, null);
-            gcNotificationListeners.put(garbageCollectorMXBean, gcNotificationListener);
-        }
-
-        registerBuildOperationListener(gradle);
-
-        gradle.addListener(new JsonAdapter(gradle));
-    }
-
-    private void registerBuildOperationListener(Gradle gradle) {
+        systemMonitoring.start(traceResult);
+        gcMonitoring.start(traceResult);
         buildOperationListener = BuildOperationListenerAdapter.create(gradle, traceResult);
+        gradle.addListener(new TraceFinalizerAdapter(gradle));
     }
 
-    private void unregisterBuildOperationListener() {
-        if (buildOperationListener != null) {
-            buildOperationListener.remove();
-            buildOperationListener = null;
-        }
-    }
-
-    private class JsonAdapter extends BuildAdapter {
+    private class TraceFinalizerAdapter extends BuildAdapter {
         private final Gradle gradle;
 
-        private JsonAdapter(Gradle gradle) {
+        private TraceFinalizerAdapter(Gradle gradle) {
             this.gradle = gradle;
         }
 
         @Override
         public void buildFinished(BuildResult result) {
-            scheduledExecutorService.shutdown();
+            systemMonitoring.stop();
+            gcMonitoring.stop();
+            buildOperationListener.remove();
 
-            for (GarbageCollectorMXBean garbageCollectorMXBean : gcNotificationListeners.keySet()) {
-                NotificationEmitter emitter = (NotificationEmitter) garbageCollectorMXBean;
-                try {
-                    emitter.removeNotificationListener(gcNotificationListeners.get(garbageCollectorMXBean));
-                } catch (ListenerNotFoundException e) {
-                }
-            }
-
-            unregisterBuildOperationListener();
-
-            traceResult.start(PHASE_BUILD, CATEGORY_PHASE, toNanoTime(buildRequestMetaData.getBuildTimeClock().getStartTime()));
+            traceResult.start(PHASE_BUILD, CATEGORY_PHASE, TimeUtil.toNanoTime(buildRequestMetaData.getBuildTimeClock().getStartTime()));
             traceResult.finish(PHASE_BUILD, System.nanoTime(), new HashMap<>());
-
             traceResult.finalizeTraceFile(result.getGradle());
 
             gradle.removeListener(this);
         }
-    }
-
-    private long toNanoTime(long timeInMillis) {
-        long elapsedMillis = System.currentTimeMillis() - timeInMillis;
-        long elapsedNanos = TimeUnit.MILLISECONDS.toNanos(elapsedMillis);
-        return System.nanoTime() - elapsedNanos;
     }
 }
--- subprojects/chrome-trace/src/main/java/org/gradle/trace/listener/BuildOperationListenerInvocationHandler.java
+++ subprojects/chrome-trace/src/main/java/org/gradle/trace/listener/BuildOperationListenerInvocationHandler.java
@@ -3,12 +3,12 @@ package org.gradle.trace.listener;
 import org.gradle.api.internal.TaskInternal;
 import org.gradle.internal.progress.OperationStartEvent;
 import org.gradle.trace.TraceResult;
+import org.gradle.trace.util.TimeUtil;
 
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.concurrent.TimeUnit;
 
 public abstract class BuildOperationListenerInvocationHandler implements InvocationHandler {
     private static final String CATEGORY_OPERATION = "BUILD_OPERATION";
@@ -25,7 +25,7 @@ public abstract class BuildOperationListenerInvocationHandler implements Invocat
             case "started": {
                 Object operation = args[0];
                 OperationStartEvent startEvent = (OperationStartEvent) args[1];
-                traceResult.start(getName(operation), CATEGORY_OPERATION, toNanoTime(startEvent.getStartTime()));
+                traceResult.start(getName(operation), CATEGORY_OPERATION, TimeUtil.toNanoTime(startEvent.getStartTime()));
                 return null;
             }
             case "finished": {
@@ -33,7 +33,7 @@ public abstract class BuildOperationListenerInvocationHandler implements Invocat
                 Object result = args[1];
                 Map<String, String> info = new HashMap<>();
                 withTaskInfo(info, getTask(operation));
-                traceResult.finish(getName(operation), toNanoTime((long) call(result, "getEndTime")), info);
+                traceResult.finish(getName(operation), TimeUtil.toNanoTime((long) call(result, "getEndTime")), info);
                 return null;
             }
             case "hashCode":
@@ -67,11 +67,5 @@ public abstract class BuildOperationListenerInvocationHandler implements Invocat
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
-    }
-
-    private long toNanoTime(long timeInMillis) {
-        long elapsedMillis = System.currentTimeMillis() - timeInMillis;
-        long elapsedNanos = TimeUnit.MILLISECONDS.toNanos(elapsedMillis);
-        return System.nanoTime() - elapsedNanos;
     }
 }
