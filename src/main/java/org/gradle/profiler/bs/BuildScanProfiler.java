43561d173e6729a325740fe1c0365b9a96426928
Lóránt Pintér <lorant.pinter@gmail.com>
2018-02-07 00:56:28 +0100

    Upgrade build scan plugin to 1.11

--- src/main/java/org/gradle/profiler/bs/BuildScanProfiler.java
+++ src/main/java/org/gradle/profiler/bs/BuildScanProfiler.java
@@ -15,7 +15,7 @@ import java.util.stream.Stream;
 
 public class BuildScanProfiler extends Profiler {
 
-    private final static String VERSION = "1.8";
+    private final static String VERSION = "1.11";
 
     private final String buildScanVersion;
 
