858eb61e27503a3d8fa00cbf6c818d75bcde487d
Stefan Oehme <stefan@gradle.com>
2019-01-14 21:29:07 +0100

    Improve error message for JVMs that don't have JFR

--- src/main/java/org/gradle/profiler/jfr/JFRJvmArgsCalculator.java
+++ src/main/java/org/gradle/profiler/jfr/JFRJvmArgsCalculator.java
@@ -4,16 +4,24 @@ import org.gradle.api.JavaVersion;
 import org.gradle.profiler.JvmArgsCalculator;
 
 import java.util.List;
+import java.util.Locale;
 
 public class JFRJvmArgsCalculator implements JvmArgsCalculator {
     @Override
     public void calculateJvmArgs(List<String> jvmArgs) {
         if (!JavaVersion.current().isJava11Compatible()) {
+            if (!isOracleVm()) {
+                throw new IllegalArgumentException("JFR is only supported on OpenJDK since Java 11 and Oracle JDK since Java 7");
+            }
             jvmArgs.add("-XX:+UnlockCommercialFeatures");
         }
         jvmArgs.add("-XX:+FlightRecorder");
         jvmArgs.add("-XX:FlightRecorderOptions=stackdepth=1024");
         jvmArgs.add("-XX:+UnlockDiagnosticVMOptions");
         jvmArgs.add("-XX:+DebugNonSafepoints");
+    }
+
+    private boolean isOracleVm() {
+        return System.getProperty("java.vendor").toLowerCase(Locale.ROOT).contains("oracle");
     }
 }
