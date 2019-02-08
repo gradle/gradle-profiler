982a9e524bbbed6558edf0e1b7ce5debeebdd50c
Kyle Koser <kkoser@linkedin.com>
2019-02-07 17:19:23 -0800

    Add mutation support for Android Layout files
    Changes to Android layout files are a common situation to profile, especially when using Android Data binding, which
    will trigger its own compiler. This PR adds support for this form of mutations to the profiler. It supports mutating
    both normal layout files and data binding layouts, which have a special format.
    
    This is done by inserting a non-visible view into the bottom of the layout with a random ID. The ID is important
    as it guanartees that the API of the data binding model for this layout will also change.
    
    The one thing not supported currently is mutating a layout file with a single element inside it, as in that case there
    is nowhere for a fake element to be inserted. Likely the solution to this is to make the fake element a wrapper layout,
    but I don't see much use for that use case right now, so I left it out.
    
    Signed-off-by: Kyle Koser <kkoser@linkedin.com>

---
+++ src/main/java/org/gradle/profiler/mutations/ApplyChangeToAndroidLayoutFileMutator.java
@@ -0,0 +1,52 @@
+package org.gradle.profiler.mutations;
+
+import java.io.File;
+
+public class ApplyChangeToAndroidLayoutFileMutator extends AbstractFileChangeMutator {
+    public ApplyChangeToAndroidLayoutFileMutator(File sourceFile) {
+        super(sourceFile);
+    }
+
+    @Override
+    protected void applyChangeTo(StringBuilder text) {
+        // We need to handle both databinding layouts, which end with </layout>, and non, which can end with any layout item closing tag
+        // Handle DataBinding first
+        int insertPos = text.lastIndexOf("</layout>");
+        if (insertPos < 0) {
+            // handle not databinding
+            applyChangeToNonDataBindingLayout(text);
+        } else {
+            applyChangeToDataBindingLayout(text, insertPos);
+        }
+    }
+
+    private void applyChangeToDataBindingLayout(StringBuilder text, int tagEndPosition) {
+        // Find the closing tag before the </layout>, which will be the closing of the top-level layout item we want to add to
+        String substring = text.substring(0, tagEndPosition);
+        int insertPos = substring.lastIndexOf("</");
+        if (insertPos < 0) {
+            throw new IllegalArgumentException("Cannot parse android layout file " + sourceFile + " to apply changes position " + insertPos);
+        }
+
+        text.insert(insertPos, generateUniqueViewItem());
+    }
+
+    private void applyChangeToNonDataBindingLayout(StringBuilder text) {
+        // For non-databinding layouts, we can just append right before the last item ends, which will be some form of a layout item
+        int insertPos = text.lastIndexOf("</");
+        if (insertPos < 0) {
+            throw new IllegalArgumentException("Cannot parse android layout file " + sourceFile + " to apply changes");
+        }
+
+        text.insert(insertPos, generateUniqueViewItem());
+    }
+
+    private String generateUniqueViewItem() {
+        return "<View \n" +
+               "    android:id=\"@+id/view" + getUniqueText() + "\"\n" +
+               "    android:visibility=\"gone\"\n" +
+               "    android:layout_width=\"5dp\"\n" +
+               "    android:layout_height=\"5dp\"/>\n" +
+               "\n";
+    }
+}
