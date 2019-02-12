507d9842edf27715423807f278702bb6a53e8083
Kyle Koser <kkoser@linkedin.com>
2019-02-12 09:15:32 -0800

    Remove inline comments as they can grow stale
    Signed-off-by: Kyle Koser <kkoser@linkedin.com>

--- src/main/java/org/gradle/profiler/mutations/ApplyChangeToAndroidLayoutFileMutator.java
+++ src/main/java/org/gradle/profiler/mutations/ApplyChangeToAndroidLayoutFileMutator.java
@@ -4,7 +4,7 @@ import java.io.File;
 
 /**
  * This class applies a mutation to an Android layout file by adding an extra, hidden view at the bottom of the layout.
- * It supports both DataBinding layouts and traditional Android layouts.
+ * It supports both DataBinding layouts (wrapped in <layout></layout> tags) and traditional Android layouts.
  *
  * Note: This mutator does not support layouts with a single view in them - it requires a valid ViewGroup to contain the
  * new view. Attempting to mutate a layout that does not have a ViewGroup as its root element will result in an
@@ -17,11 +17,8 @@ public class ApplyChangeToAndroidLayoutFileMutator extends AbstractFileChangeMut
 
     @Override
     protected void applyChangeTo(StringBuilder text) {
-        // We need to handle both databinding layouts, which end with </layout>, and non, which can end with any layout item closing tag
-        // Handle DataBinding first
         int insertPos = text.lastIndexOf("</layout>");
         if (insertPos < 0) {
-            // handle not databinding
             applyChangeToNonDataBindingLayout(text);
         } else {
             applyChangeToDataBindingLayout(text, insertPos);
@@ -29,7 +26,6 @@ public class ApplyChangeToAndroidLayoutFileMutator extends AbstractFileChangeMut
     }
 
     private void applyChangeToDataBindingLayout(StringBuilder text, int tagEndPosition) {
-        // Find the closing tag before the </layout>, which will be the closing of the top-level layout item we want to add to
         String substring = text.substring(0, tagEndPosition);
         int insertPos = substring.lastIndexOf("</");
         if (insertPos < 0) {
@@ -40,7 +36,6 @@ public class ApplyChangeToAndroidLayoutFileMutator extends AbstractFileChangeMut
     }
 
     private void applyChangeToNonDataBindingLayout(StringBuilder text) {
-        // For non-databinding layouts, we can just append right before the last item ends, which will be some form of a layout item
         int insertPos = text.lastIndexOf("</");
         if (insertPos < 0) {
             throw new IllegalArgumentException("Cannot parse android layout file " + sourceFile + " to apply changes");
