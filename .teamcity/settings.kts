24bfb16187535f3088aea0f6fd762f8c586e35f7
wolf <wolf@TeamCity>
2019-07-29 10:52:37 +0200

    TeamCity change in 'Gradle Profiler' project: Versioned settings configuration updated

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,6 +1,8 @@
-import jetbrains.buildServer.configs.kotlin.v2018_2.project
+import jetbrains.buildServer.configs.kotlin.v2018_2.*
+import jetbrains.buildServer.configs.kotlin.v2018_2.buildFeatures.commitStatusPublisher
+import jetbrains.buildServer.configs.kotlin.v2018_2.buildSteps.gradle
+import jetbrains.buildServer.configs.kotlin.v2018_2.triggers.vcs
 import jetbrains.buildServer.configs.kotlin.v2018_2.vcs.GitVcsRoot
-import jetbrains.buildServer.configs.kotlin.v2018_2.version
 
 /*
 The settings script is an entry point for defining a TeamCity
@@ -33,7 +35,192 @@ project {
 
     buildType(MacOSJava18)
     buildType(WindowsJava18)
-    buildType(LinuxJava18)
-
     buildType(GradleProfilerPublishing)
+    buildType(LinuxJava18)
 }
+
+object GradleProfilerPublishing : BuildType({
+    name = "Gradle profiler Publishing"
+    description = "Publish Gradle profiler Gradle's Artifactory repository"
+
+    artifactRules = "build/reports/** => reports"
+
+    params {
+        param("env.JAVA_HOME", "%linux.java8.oracle.64bit%")
+        text("ARTIFACTORY_USERNAME", "bot-build-tool", allowEmpty = true)
+        password("ARTIFACTORY_PASSWORD", "credentialsJSON:2b7529cd-77cd-49f4-9416-9461f6ac9018", display = ParameterDisplay.HIDDEN)
+    }
+
+    vcs {
+        root(DslContext.settingsRoot)
+
+        checkoutMode = CheckoutMode.ON_SERVER
+    }
+
+    steps {
+        gradle {
+            tasks = "clean publishToGradleBuildInternal"
+            buildFile = ""
+            gradleParams = "-PartifactoryUsername=%ARTIFACTORY_USERNAME% -PartifactoryPassword=%ARTIFACTORY_PASSWORD%"
+            param("org.jfrog.artifactory.selectedDeployableServer.defaultModuleVersionConfiguration", "GLOBAL")
+        }
+    }
+
+    requirements {
+        contains("teamcity.agent.jvm.os.name", "Linux")
+    }
+})
+
+object LinuxJava18 : BuildType({
+    name = "Linux - Java 1.8"
+
+    params {
+        param("env.JAVA_HOME", "%linux.java8.oracle.64bit%")
+    }
+
+    vcs {
+        root(DslContext.settingsRoot)
+
+        checkoutMode = CheckoutMode.ON_SERVER
+    }
+
+    steps {
+        gradle {
+            tasks = "clean build"
+            buildFile = ""
+            gradleParams = "-s"
+            param("org.jfrog.artifactory.selectedDeployableServer.defaultModuleVersionConfiguration", "GLOBAL")
+        }
+    }
+
+    triggers {
+        vcs {
+            branchFilter = """
+                +:*
+                -:pull/*
+            """.trimIndent()
+        }
+    }
+
+    features {
+        commitStatusPublisher {
+            vcsRootExtId = "${DslContext.settingsRoot.id}"
+            publisher = github {
+                githubUrl = "https://api.github.com"
+                authType = personalToken {
+                    token = "credentialsJSON:7c8c64ad-776d-4469-8400-5618da5de337"
+                }
+            }
+        }
+    }
+
+    requirements {
+        contains("teamcity.agent.jvm.os.name", "Linux")
+    }
+})
+
+object MacOSJava18 : BuildType({
+    name = "MacOS - Java 1.8"
+
+    params {
+        param("env.JAVA_HOME", "%macos.java8.oracle.64bit%")
+    }
+
+    vcs {
+        root(DslContext.settingsRoot)
+
+        checkoutMode = CheckoutMode.ON_SERVER
+    }
+
+    steps {
+        gradle {
+            tasks = "clean build"
+            buildFile = ""
+            gradleParams = "-s"
+            param("org.jfrog.artifactory.selectedDeployableServer.defaultModuleVersionConfiguration", "GLOBAL")
+        }
+    }
+
+    triggers {
+        vcs {
+            branchFilter = """
+                +:*
+                -:pull/*
+            """.trimIndent()
+        }
+    }
+
+    features {
+        commitStatusPublisher {
+            vcsRootExtId = "${DslContext.settingsRoot.id}"
+            publisher = github {
+                githubUrl = "https://api.github.com"
+                authType = personalToken {
+                    token = "credentialsJSON:7c8c64ad-776d-4469-8400-5618da5de337"
+                }
+            }
+        }
+    }
+
+    requirements {
+        contains("teamcity.agent.jvm.os.name", "Mac OS X")
+    }
+})
+
+object WindowsJava18 : BuildType({
+    name = "Windows - Java 1.8"
+
+    params {
+        param("env.JAVA_HOME", "%windows.java8.oracle.64bit%")
+    }
+
+    vcs {
+        root(DslContext.settingsRoot)
+
+        checkoutMode = CheckoutMode.ON_SERVER
+    }
+
+    steps {
+        gradle {
+            tasks = "clean build"
+            buildFile = ""
+            gradleParams = "-s"
+            param("org.jfrog.artifactory.selectedDeployableServer.defaultModuleVersionConfiguration", "GLOBAL")
+        }
+    }
+
+    triggers {
+        vcs {
+            branchFilter = """
+                +:*
+                -:pull/*
+            """.trimIndent()
+        }
+    }
+
+    features {
+        commitStatusPublisher {
+            vcsRootExtId = "${DslContext.settingsRoot.id}"
+            publisher = github {
+                githubUrl = "https://api.github.com"
+                authType = personalToken {
+                    token = "credentialsJSON:7c8c64ad-776d-4469-8400-5618da5de337"
+                }
+            }
+        }
+    }
+
+    requirements {
+        contains("teamcity.agent.jvm.os.name", "Windows")
+    }
+})
+
+object VersionedSettings_1 : GitVcsRoot({
+    id("VersionedSettings")
+    name = "Gradle Profiler Versioned Settings"
+    url = "git@github.com:gradle/gradle-profiler.git"
+    branch = "teamcity-versioned-settings"
+    authMethod = uploadedKey {
+        uploadedKey = "id_rsa_gradlewaregitbot"
+    }
+})
