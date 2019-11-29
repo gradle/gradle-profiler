258abd1aa0a9834f728b2b1d89fa7b0f275b0073
Lóránt Pintér <lorant.pinter@gmail.com>
2019-11-29 14:40:37 +0100

    Safer file deletion for Windows

--- src/main/java/org/gradle/profiler/mutations/AbstractCleanupMutator.java
+++ src/main/java/org/gradle/profiler/mutations/AbstractCleanupMutator.java
@@ -1,7 +1,6 @@
 package org.gradle.profiler.mutations;
 
 import com.typesafe.config.Config;
-import org.apache.commons.io.FileUtils;
 import org.gradle.profiler.BuildContext;
 import org.gradle.profiler.BuildMutator;
 import org.gradle.profiler.ConfigUtil;
@@ -9,7 +8,9 @@ import org.gradle.profiler.ScenarioContext;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.UncheckedIOException;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.function.Supplier;
 
 public abstract class AbstractCleanupMutator implements BuildMutator {
@@ -43,15 +44,51 @@ public abstract class AbstractCleanupMutator implements BuildMutator {
 
     abstract protected void cleanup();
 
-    protected static void delete(File f) {
-        try {
-            if (f.isFile()) {
-                Files.delete(f.toPath());
-            } else {
-                FileUtils.deleteDirectory(f);
+    protected static void delete(File file) {
+        if (file.isDirectory()) {
+            File[] contents = file.listFiles();
+
+            // Something else may have removed it
+            if (contents == null) {
+                return;
             }
-        } catch (IOException e) {
-            throw new IllegalStateException("Could not delete " + f, e);
+
+            for (File item : contents) {
+                delete(item);
+            }
+        }
+
+        try {
+            tryHardToDelete(file.toPath());
+        } catch (IOException ex) {
+            throw new UncheckedIOException(String.format("Couldn't delete file '%s'", file), ex);
+        }
+    }
+
+    private static void tryHardToDelete(Path path) throws IOException {
+        try {
+            Files.deleteIfExists(path);
+            if (!Files.exists(path)) {
+                return;
+            }
+        } catch (IOException ex) {
+            // Continue
+        }
+
+        // This is copied from Ant (see org.apache.tools.ant.util.FileUtils.tryHardToDelete).
+        // It mentions that there is a bug in the Windows JDK implementations that this is a valid
+        // workaround for. I've been unable to find a definitive reference to this bug.
+        // The thinking is that if this is good enough for Ant, it's good enough for us.
+        System.gc();
+        try {
+            Thread.sleep(100);
+        } catch (InterruptedException ex) {
+            Thread.currentThread().interrupt();
+        }
+
+        Files.deleteIfExists(path);
+        if (Files.exists(path)) {
+            throw new IOException(String.format("File '%s' still exists after repeated attempts to delete it", path));
         }
     }
 
