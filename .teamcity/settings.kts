4777c4938b53aabbd2be2a130d0650615a2b8abe
Stefan Wolf <wolf@gradle.com>
2020-07-13 18:09:29 +0200

    Apply ktlint and fix violations

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,5 +1,4 @@
 import jetbrains.buildServer.configs.kotlin.v2019_2.project
-import jetbrains.buildServer.configs.kotlin.v2019_2.vcs.GitVcsRoot
 import jetbrains.buildServer.configs.kotlin.v2019_2.version
 
 /*
