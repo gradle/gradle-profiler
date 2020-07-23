665805db78f423798d7b106800e414aa24378e22
Stefan Wolf <wolf@gradle.com>
2020-07-23 17:59:16 +0200

    Adjust Teamcity configuration

--- .teamcity/settings/GradleProfilerTest.kt
+++ .teamcity/settings/GradleProfilerTest.kt
@@ -15,7 +15,7 @@ open class GradleProfilerTest(os: Os) : BuildType({
         gradle {
             tasks = "clean build"
             buildFile = ""
-            gradleParams = "-s $useGradleInternalScansServer"
+            gradleParams = "-s"
             param("org.jfrog.artifactory.selectedDeployableServer.defaultModuleVersionConfiguration", "GLOBAL")
         }
     }
