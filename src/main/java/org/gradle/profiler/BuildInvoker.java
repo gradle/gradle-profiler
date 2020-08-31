465f954c6882834db622cb74fc8b0349efdfeed9
Lóránt Pintér <lorant.pinter@gmail.com>
2020-08-31 14:05:20 +0200

    Make BuildInvoker public

--- src/main/java/org/gradle/profiler/BuildInvoker.java
+++ src/main/java/org/gradle/profiler/BuildInvoker.java
@@ -1,19 +1,19 @@
 package org.gradle.profiler;
 
 public class BuildInvoker {
-    static final BuildInvoker Bazel = new BuildInvoker() {
+    public static final BuildInvoker Bazel = new BuildInvoker() {
         @Override
         public String toString() {
             return "Bazel";
         }
     };
-    static final BuildInvoker Buck = new BuildInvoker() {
+    public static final BuildInvoker Buck = new BuildInvoker() {
         @Override
         public String toString() {
             return "Buck";
         }
     };
-    static final BuildInvoker Maven = new BuildInvoker() {
+    public static final BuildInvoker Maven = new BuildInvoker() {
         @Override
         public String toString() {
             return "Maven";
