39ff102565cc163fd7ece23d765560477005dfbc
Lóránt Pintér <lorant.pinter@gmail.com>
2020-09-01 11:25:58 +0200

    Revert "Expose projectDir in BuildContext and ScenarioContext"
    This reverts commit b39a9cb9fa259595d7c138e194619e029793a14a.

--- src/main/java/org/gradle/profiler/ScenarioContext.java
+++ src/main/java/org/gradle/profiler/ScenarioContext.java
@@ -1,15 +1,11 @@
 package org.gradle.profiler;
 
-import java.io.File;
-
 public interface ScenarioContext {
     static ScenarioContext from(InvocationSettings invocationSettings, ScenarioDefinition scenarioDefinition) {
-        return new DefaultScenarioContext(invocationSettings.getInvocationId(), scenarioDefinition.getName(), invocationSettings.getProjectDir());
+        return new DefaultScenarioContext(invocationSettings.getInvocationId(), scenarioDefinition.getName());
     }
 
     String getUniqueScenarioId();
-
-    File getProjectDir();
 
     BuildContext withBuild(Phase phase, int count);
 }
