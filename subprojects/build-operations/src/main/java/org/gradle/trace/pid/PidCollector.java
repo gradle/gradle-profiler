42064e7cbeb6b0a73899a26daffaad152258fc64
Stefan Wolf <wolf@gradle.com>
2020-09-04 11:20:12 +0200

    Fix PidCollector for TAPI model requests.

--- subprojects/build-operations/src/main/java/org/gradle/trace/pid/PidCollector.java
+++ subprojects/build-operations/src/main/java/org/gradle/trace/pid/PidCollector.java
@@ -10,11 +10,11 @@ import java.io.File;
 @SuppressWarnings("unused")
 public class PidCollector {
     public static void collect(GradleInternal gradle, File outFile) {
+        // Always write the file, whether we register a build service or not, so we also have a PID file for TAPI model requests.
+        ProcessEnvironment processEnvironment = gradle.getServices().get(ProcessEnvironment.class);
+        GFileUtils.writeFile(processEnvironment.getPid().toString(), outFile);
         if (GradleVersion.current().compareTo(GradleVersion.version("6.1")) >= 0) {
             PidCollectorBuildService.registerBuildService(gradle, outFile);
-        } else {
-            ProcessEnvironment processEnvironment = gradle.getServices().get(ProcessEnvironment.class);
-            GFileUtils.writeFile(processEnvironment.getPid().toString(), outFile);
         }
     }
 }
