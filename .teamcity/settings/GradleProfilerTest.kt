39e2ba7fdaad893b01110ccc4cfe80f5524ffc5f
Stefan Wolf <wolf@gradle.com>
2020-12-18 11:05:42 +0100

    Revert "Switch Linux Java 8 build to Java 11 to try it out"
    This reverts commit 63366db9dc0fc29bd0bcf08fbd0aada52f11c478.

--- .teamcity/settings/GradleProfilerTest.kt
+++ .teamcity/settings/GradleProfilerTest.kt
@@ -63,7 +63,7 @@ open class GradleProfilerTest(os: Os, javaVersion: JavaVersion) : BuildType({
     }
 }
 
-object LinuxJava18 : GradleProfilerTest(Os.linux, JavaVersion.OPENJDK_11, {
+object LinuxJava18 : GradleProfilerTest(Os.linux, JavaVersion.ORACLE_JAVA_8, {
     name = "Linux - Java 1.8"
 })
 
