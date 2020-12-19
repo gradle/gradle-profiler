394869c073905b2ceef90fd3853646b0652da5ba
Stefan Wolf <wolf@gradle.com>
2020-12-19 23:36:03 +0100

    Add operating system as input to test task

--- buildSrc/src/main/kotlin/profiler.java-library.gradle.kts
+++ buildSrc/src/main/kotlin/profiler.java-library.gradle.kts
@@ -1,3 +1,5 @@
+import org.gradle.internal.os.OperatingSystem
+
 plugins {
     id("java-library")
 }
@@ -21,4 +23,10 @@ abstract class Versions {
     val toolingApi = "org.gradle:gradle-tooling-api:6.6.1"
     val groovy = "org.codehaus.groovy:groovy:2.4.7"
     val spock = "org.spockframework:spock-core:1.1-groovy-2.4"
+}
+
+tasks.withType<Test>().configureEach {
+    // Add OS as inputs since tests on different OS may behave differently https://github.com/gradle/gradle-private/issues/2831
+    // the version currently differs between our dev infrastructure, so we only track the name and the architecture
+    inputs.property("operatingSystem", "${OperatingSystem.current().name} ${System.getProperty("os.arch")}")
 }
