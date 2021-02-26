fb8fe9c0011c8bd6bb8f0155d5ef8464a71894f3
Sterling Greene <sterling@gradle.com>
2021-02-26 17:33:50 -0500

    Accept both names for Lambda classes
    . and / are used depending on JVM version

--- src/test/groovy/org/gradle/profiler/flamegraph/FlameGraphSanitizerTest.groovy
+++ src/test/groovy/org/gradle/profiler/flamegraph/FlameGraphSanitizerTest.groovy
@@ -6,6 +6,7 @@ class FlameGraphSanitizerTest extends Specification {
     def "normalizes lambda stack frames"() {
         def normalizer = FlameGraphSanitizer.NORMALIZE_LAMBDA_NAMES
         expect:
+        normalizer.map(['DefaultPlanExecutor$ExecutorWorker$$Lambda$887.1827771163.execute']) == ['DefaultPlanExecutor$ExecutorWorker$$Lambda$.execute']
         normalizer.map(['DefaultPlanExecutor$ExecutorWorker$$Lambda$887/1827771163.execute']) == ['DefaultPlanExecutor$ExecutorWorker$$Lambda$.execute']
     }
 }
