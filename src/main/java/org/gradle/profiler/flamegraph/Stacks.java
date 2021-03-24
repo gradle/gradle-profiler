73ea9ba316e306235fcb6cc2cf9dcfd7198b54c9
Stefan Wolf <wolf@gradle.com>
2021-03-24 13:37:59 +0100

    Separate differential stacks and graphs generation

--- src/main/java/org/gradle/profiler/flamegraph/Stacks.java
+++ src/main/java/org/gradle/profiler/flamegraph/Stacks.java
@@ -10,16 +10,22 @@ public class Stacks {
     private final EventType type;
     private final DetailLevel level;
     private final String fileBaseName;
+    private final boolean negate;
 
     public static String postFixFor(EventType type, DetailLevel level) {
         return "-" + type.getId() + "-" + level.name().toLowerCase(Locale.ROOT);
     }
 
     public Stacks(File file, EventType type, DetailLevel level, String fileBaseName) {
+        this(file, type, level, fileBaseName, false);
+    }
+
+    public Stacks(File file, EventType type, DetailLevel level, String fileBaseName, boolean negate) {
         this.file = file;
         this.type = type;
         this.level = level;
         this.fileBaseName = fileBaseName;
+        this.negate = negate;
     }
 
     public File getFile() {
@@ -40,5 +46,9 @@ public class Stacks {
 
     public String getFileBaseName() {
         return fileBaseName;
+    }
+
+    public boolean isNegate() {
+        return negate;
     }
 }
