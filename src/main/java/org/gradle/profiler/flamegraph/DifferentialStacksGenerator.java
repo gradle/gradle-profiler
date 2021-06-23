98e43dcd8f504fdb28d099d98fe27d16b81b9ced
Stefan Wolf <wolf@gradle.com>
2021-06-23 17:31:17 +0200

    Generate differential diff in Java
    The generation is simple enough, so we can implement it in Java
    and don't need to rely on it for the stacks generation.

--- src/main/java/org/gradle/profiler/flamegraph/DifferentialStacksGenerator.java
+++ src/main/java/org/gradle/profiler/flamegraph/DifferentialStacksGenerator.java
@@ -3,8 +3,11 @@ package org.gradle.profiler.flamegraph;
 import com.google.common.base.Joiner;
 
 import javax.annotation.Nullable;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.IOException;
+import java.io.UncheckedIOException;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayDeque;
@@ -12,7 +15,12 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.Deque;
+import java.util.HashSet;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.function.Consumer;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -20,12 +28,8 @@ import java.util.stream.Stream;
  * Generates differential stacks.
  */
 public class DifferentialStacksGenerator {
-    private final FlameGraphTool flameGraphTool = new FlameGraphTool();
 
     public List<Stacks> generateDifferentialStacks(File baseOutputDir) throws IOException {
-        if (!flameGraphTool.checkInstallation()) {
-            return Collections.emptyList();
-        }
         List<Stacks> stacks = new ArrayList<>();
         try (Stream<Path> list = Files.list(baseOutputDir.toPath())) {
             List<Path> experiments = list
@@ -64,15 +68,64 @@ public class DifferentialStacksGenerator {
             File diff = new File(underTestStacks.getParentFile(), "diffs/" + diffBaseName + Stacks.STACKS_FILE_SUFFIX);
             diff.getParentFile().mkdirs();
             if (negate) {
-                flameGraphTool.generateDiff(underTestStacks, baselineStacks, diff);
+                generateDiff(underTestStacks, baselineStacks, diff);
             } else {
-                flameGraphTool.generateDiff(baselineStacks, underTestStacks, diff);
+                generateDiff(baselineStacks, underTestStacks, diff);
             }
 
             return new Stacks(diff, type, level, diffBaseName, negate);
         }
 
         return null;
+    }
+
+    public void generateDiff(File versionUnderTest, File baseline, File diff) {
+        try {
+            Map<String, Long> parsedUnderTestStacks = readStacks(versionUnderTest);
+            Map<String, Long> parsedBaselineStacks = readStacks(baseline);
+            try (BufferedWriter writer = Files.newBufferedWriter(diff.toPath(), StandardCharsets.UTF_8)) {
+                generateDiff(parsedUnderTestStacks, parsedBaselineStacks, stackLine -> {
+                    try {
+                        writer.write(stackLine);
+                        writer.newLine();
+                    } catch (IOException e) {
+                        throw new UncheckedIOException(e);
+                    }
+                });
+            }
+        } catch (Exception e) {
+            System.err.printf("Problem while creating differential stack file %s: %s%n", diff, e.getMessage());
+        }
+    }
+
+    private Map<String, Long> readStacks(File file) throws IOException {
+        try (Stream<String> lines = Files.lines(file.toPath(), StandardCharsets.UTF_8)) {
+            Map<String, Long> result = new LinkedHashMap<>();
+            lines.forEach(line -> {
+                if (line.isEmpty()) {
+                    return;
+                }
+                int lastSpace = line.lastIndexOf(" ");
+                if (lastSpace == -1) {
+                    return;
+                }
+                String stack = line.substring(0, lastSpace).trim();
+                long count = Long.parseLong(line.substring(lastSpace + 1));
+                result.compute(stack, (s, c) -> c == null ? count : c + count);
+            });
+            return result;
+        }
+    }
+
+    private void generateDiff(Map<String, Long> from, Map<String, Long> to, Consumer<String> stackConsumer) {
+        Set<String> allStacks = new HashSet<>();
+        allStacks.addAll(from.keySet());
+        allStacks.addAll(to.keySet());
+        for (String stack : allStacks) {
+            long countFrom = from.getOrDefault(stack, 0L);
+            long countTo = to.getOrDefault(stack, 0L);
+            stackConsumer.accept(stack + " " + countFrom + " " + countTo);
+        }
     }
 
     private String computeDifferenceOfBaselineToCurrentName(String underTestBasename, String baselineTestBasename) {
