6836d3351a09618d17f1958cdfe97612247bae07
Anže Sodja <asodja@gradle.com>
2021-11-08 14:41:05 +0100

    Set JAVA_HOME env variable for test

--- build.gradle.kts
+++ build.gradle.kts
@@ -115,6 +115,8 @@ tasks.test {
         }.getOrNull()
     }
     javaLauncher.set(launcher)
+    // So processes started from test also use same Java version
+    environment = mapOf("JAVA_HOME" to javaLauncher.get().metadata.installationPath)
 
     // We had some build failures on macOS, where it seems to be a Socket was already closed when trying to download the Gradle distribution.
     // The tests failing were consistenly in ProfilerIntegrationTest.
