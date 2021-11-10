e9c920641209c2a58a4d0dca606fe9a69d6e50f8
Anže Sodja <asodja@gradle.com>
2021-11-10 21:30:17 +0100

    Move combinations code to a separate class

--- src/main/java/org/gradle/profiler/mutations/ApplyDependencyGraphMutator.java
+++ src/main/java/org/gradle/profiler/mutations/ApplyDependencyGraphMutator.java
@@ -1,7 +1,5 @@
 package org.gradle.profiler.mutations;
 
-import com.google.common.collect.Sets;
-import com.google.common.math.IntMath;
 import com.typesafe.config.Config;
 import org.apache.commons.io.FileUtils;
 import org.gradle.profiler.BuildContext;
@@ -12,34 +10,28 @@ import org.gradle.profiler.InvocationSettings;
 import org.gradle.profiler.ScenarioContext;
 import org.gradle.profiler.ScenarioDefinition;
 import org.gradle.profiler.mutations.support.FileSupport;
+import org.gradle.profiler.mutations.support.ProjectCombinations;
 
 import java.io.File;
 import java.nio.file.StandardOpenOption;
-import java.util.Iterator;
-import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Set;
-import java.util.concurrent.atomic.AtomicLong;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 import java.util.stream.Stream;
+
+import static org.gradle.profiler.mutations.support.ProjectCombinationsSupport.createProjectCombinations;
 
 public class ApplyDependencyGraphMutator extends AbstractFileChangeMutator {
 
-    /**
-     * From 34 choose 17 = 2333606220 that is more than an integer
-     * From 33 choose 16 = 1166803110 that is less than an integer, so max n is 33
-     */
-    private static final int MAX_N = 33;
-
     private final InvocationSettings settings;
-    private final long mutatorIndex;
+    private final int mutatorIndex;
     private final File generatedProjectsDir;
     private final File settingsFile;
     private final String originalSettingsText;
     private ProjectCombinations projectCombinations;
 
-    protected ApplyDependencyGraphMutator(File sourceFile, InvocationSettings settings, long mutatorIndex) {
+    protected ApplyDependencyGraphMutator(File sourceFile, InvocationSettings settings, int mutatorIndex) {
         super(sourceFile);
         this.settings = settings;
         this.mutatorIndex = mutatorIndex;
@@ -67,43 +59,18 @@ public class ApplyDependencyGraphMutator extends AbstractFileChangeMutator {
     public void beforeScenario(ScenarioContext context) {
         ScenarioDefinition scenario = context.getScenarioDefinition();
         int numberOfIterations = scenario.getWarmUpCount() + scenario.getBuildCount();
-        projectCombinations = createProjectCombinations(numberOfIterations);
+        projectCombinations = createProjectCombinations(mutatorIndex, numberOfIterations);
 
         createProjects(projectCombinations.getProjectNames());
-        String additionalProjects = projectCombinations.getProjectNames().stream()
-            .map(it -> "\ninclude(\"" + it + "\")")
+        String includeProjects = projectCombinations.getProjectNames().stream()
+            .map(it -> {
+                String include = String.format("\ninclude(\"%s\")", it);
+                String projectPath = new File(generatedProjectsDir.getName(), it).toString();
+                String projectDir = String.format("\nproject(\":%s\").projectDir = file(\"%s\")", it, projectPath);
+                return include + projectDir;
+            })
             .collect(Collectors.joining());
-        FileSupport.writeUnchecked(settingsFile.toPath(), additionalProjects, StandardOpenOption.APPEND);
-    }
-
-    /**
-     * We use combination of projects. We don't try all combinations, but just combinations that satisfies:
-     * "from n choose n / 2". For example if we have 15 projects, we will do combinations of 7 projects. So from
-     * 15 we will choose 7 that results in 6435 combinations.
-     */
-    public ProjectCombinations createProjectCombinations(int numberOfIterations) {
-        int numberOfProjects = findNumberOfProjects(numberOfIterations);
-        List<String> projectNames = IntStream.range(0, numberOfProjects)
-            .mapToObj(it -> String.format("project-%d-%d", mutatorIndex, it))
-            .collect(Collectors.toList());
-        int numberOfProjectsInOneCombination = getNumberOfProjectsInOneCombination(projectNames.size());
-        Set<Set<String>> combinations = Sets.combinations(new LinkedHashSet<>(projectNames), numberOfProjectsInOneCombination);
-        return new ProjectCombinations(projectNames, combinations);
-    }
-
-    private int findNumberOfProjects(int numberOfIterations) {
-        for (int n = 1; n <= MAX_N; n++) {
-            int k = getNumberOfProjectsInOneCombination(n);
-            int combinations = IntMath.binomial(n, k);
-            if (combinations >= numberOfIterations) {
-                return n;
-            }
-        }
-        throw new IllegalStateException("Too many warm up count and build count set");
-    }
-
-    private int getNumberOfProjectsInOneCombination(int n) {
-        return n <= 1 ? 1 : n / 2;
+        FileSupport.writeUnchecked(settingsFile.toPath(), includeProjects, StandardOpenOption.APPEND);
     }
 
     private void createProjects(List<String> projectNames) {
@@ -118,8 +85,8 @@ public class ApplyDependencyGraphMutator extends AbstractFileChangeMutator {
     @Override
     protected void applyChangeTo(BuildContext context, StringBuilder text) {
         text.append("\ndependencies {\n");
-        projectCombinations.getCombinationsIterator().next()
-            .forEach(it -> text.append("    project(\":").append(it).append("\")\n"));
+        Set<String> projects = projectCombinations.getCombinationsIterator().next();
+        projects.forEach(it -> text.append("    project(\":").append(it).append("\")\n"));
         text.append("\n}");
     }
 
@@ -130,32 +97,13 @@ public class ApplyDependencyGraphMutator extends AbstractFileChangeMutator {
         FileUtils.deleteQuietly(generatedProjectsDir);
     }
 
-    private static class ProjectCombinations {
-
-        private final List<String> projectNames;
-        private final Iterator<Set<String>> combinations;
-
-        public ProjectCombinations(List<String> projectNames, Set<Set<String>> combinations) {
-            this.projectNames = projectNames;
-            this.combinations = combinations.iterator();
-        }
-
-        public List<String> getProjectNames() {
-            return projectNames;
-        }
-
-        public Iterator<Set<String>> getCombinationsIterator() {
-            return combinations;
-        }
-    }
-
     public static class Configurator extends FileChangeMutatorConfigurator {
 
-        private final AtomicLong createdMutatorsCounter;
+        private final AtomicInteger createdMutatorsCounter;
 
         public Configurator() {
             super(ApplyDependencyGraphMutator.class);
-            this.createdMutatorsCounter = new AtomicLong();
+            this.createdMutatorsCounter = new AtomicInteger();
         }
 
         @Override
--- src/main/java/org/gradle/profiler/mutations/support/ProjectCombinations.java
+++ src/main/java/org/gradle/profiler/mutations/support/ProjectCombinations.java
@@ -1,4 +1,24 @@
 package org.gradle.profiler.mutations.support;
 
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
+
 public class ProjectCombinations {
+
+    private final List<String> projectNames;
+    private final Iterator<Set<String>> combinations;
+
+    public ProjectCombinations(List<String> projectNames, Set<Set<String>> combinations) {
+        this.projectNames = projectNames;
+        this.combinations = combinations.iterator();
+    }
+
+    public List<String> getProjectNames() {
+        return projectNames;
+    }
+
+    public Iterator<Set<String>> getCombinationsIterator() {
+        return combinations;
+    }
 }
