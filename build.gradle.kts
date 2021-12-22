8b60a54f485393fe8c232ba1bbdcd387b1df2174
Anže Sodja <asodja@gradle.com>
2021-12-22 11:58:27 +0100

    Remove star imports

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,9 @@
-
 import com.moowork.gradle.node.npm.NpxTask
 import io.sdkman.vendors.tasks.SdkAnnounceVersionTask
 import io.sdkman.vendors.tasks.SdkDefaultVersionTask
 import io.sdkman.vendors.tasks.SdkReleaseVersionTask
 import io.sdkman.vendors.tasks.SdkmanVendorBaseTask
-import java.util.*
+import java.util.Locale
 
 plugins {
     id("profiler.java-library")
--- buildSrc/src/main/kotlin/tasks/InstallAndroidSdkTask.kt
+++ buildSrc/src/main/kotlin/tasks/InstallAndroidSdkTask.kt
@@ -4,7 +4,11 @@ import org.gradle.api.DefaultTask
 import org.gradle.api.GradleException
 import org.gradle.api.file.DirectoryProperty
 import org.gradle.api.provider.Property
-import org.gradle.api.tasks.*
+import org.gradle.api.tasks.Input
+import org.gradle.api.tasks.Optional
+import org.gradle.api.tasks.OutputDirectory
+import org.gradle.api.tasks.TaskAction
+import org.gradle.api.tasks.UntrackedTask
 import org.gradle.tooling.GradleConnector
 import java.io.File
 
