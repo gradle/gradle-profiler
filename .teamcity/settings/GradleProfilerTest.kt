e31563a863668ba5f957284b11bda3aeedd51730
Anže Sodja <asodja@gradle.com>
2022-08-04 12:09:15 +0200

    Use OpenJDK 8 instead of Oracle for Java 8 testing

--- .teamcity/settings/GradleProfilerTest.kt
+++ .teamcity/settings/GradleProfilerTest.kt
@@ -60,7 +60,7 @@ open class GradleProfilerTest(os: Os, javaVersion: JavaVersion) : BuildType({
     }
 }
 
-object LinuxJava8 : GradleProfilerTest(Os.linux, JavaVersion.ORACLE_JAVA_8, {
+object LinuxJava8 : GradleProfilerTest(Os.linux, JavaVersion.OPENJDK_8, {
     name = "Linux - Java 8"
 })
 
@@ -68,10 +68,10 @@ object LinuxJava11 : GradleProfilerTest(Os.linux, JavaVersion.OPENJDK_11, {
     name = "Linux - Java 11"
 })
 
-object MacOSJava8 : GradleProfilerTest(Os.macos, JavaVersion.ORACLE_JAVA_8, {
+object MacOSJava8 : GradleProfilerTest(Os.macos, JavaVersion.OPENJDK_8, {
     name = "MacOS - Java 8"
 })
 
-object WindowsJava8 : GradleProfilerTest(Os.windows, JavaVersion.ORACLE_JAVA_8, {
+object WindowsJava8 : GradleProfilerTest(Os.windows, JavaVersion.OPENJDK_8, {
     name = "Windows - Java 8"
 })
