2160086c2c166fa72cd81322724a7c6b47acfd95
Anže Sodja <asodja@gradle.com>
2022-08-29 14:29:28 +0200

    Also disable resolve of external annotations on all new linked projects
    This is needed for headless mode, since projects are linked later

--- subprojects/studio-plugin/src/main/java/org/gradle/profiler/studio/plugin/GradleProfilerProjectManagerListener.java
+++ subprojects/studio-plugin/src/main/java/org/gradle/profiler/studio/plugin/GradleProfilerProjectManagerListener.java
@@ -4,16 +4,20 @@ import com.android.tools.idea.gradle.project.GradleProjectInfo;
 import com.intellij.ide.impl.TrustedProjects;
 import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.diagnostic.Logger;
+import com.intellij.openapi.externalSystem.settings.ExternalSystemSettingsListenerAdapter;
+import com.intellij.openapi.project.Project;
 import com.intellij.openapi.project.ProjectManagerListener;
 import org.gradle.profiler.client.protocol.Client;
 import org.gradle.profiler.client.protocol.messages.StudioRequest;
 import org.gradle.profiler.studio.plugin.client.GradleProfilerClient;
 import org.gradle.profiler.studio.plugin.system.AndroidStudioSystemHelper;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.plugins.gradle.settings.GradleProjectSettings;
 import org.jetbrains.plugins.gradle.settings.GradleSettings;
 
 import java.io.IOException;
 import java.io.UncheckedIOException;
+import java.util.Collection;
 
 import static org.gradle.profiler.client.protocol.messages.StudioRequest.StudioRequestType.EXIT_IDE;
 
@@ -24,7 +28,7 @@ public class GradleProfilerProjectManagerListener implements ProjectManagerListe
     public static final String PROFILER_PORT_PROPERTY = "gradle.profiler.port";
 
     @Override
-    public void projectOpened(@NotNull com.intellij.openapi.project.Project project) {
+    public void projectOpened(@NotNull Project project) {
         LOG.info("Project opened");
         if (System.getProperty(PROFILER_PORT_PROPERTY) != null) {
             // This solves the issue where Android Studio would run the Gradle sync automatically on the first import.
@@ -32,10 +36,9 @@ public class GradleProfilerProjectManagerListener implements ProjectManagerListe
             // a case where two simultaneous syncs would be run: one from automatic sync trigger and one from our trigger.
             // With this line we disable that automatic sync, but we still trigger our sync later in the code.
             GradleProjectInfo.getInstance(project).setSkipStartupActivity(true);
-            // If we don't disable this, Android Studio will download to .m2 folder if
-            // some project has com.fasterxml.jackson.core:jackson-core as a dependency
-            GradleSettings.getInstance(project).getLinkedProjectsSettings()
-                .forEach(settings -> settings.setResolveExternalAnnotations(false));
+            // If we don't disable external annotations, Android Studio will download some artifacts
+            // to .m2 folder if some project has for example com.fasterxml.jackson.core:jackson-core as a dependency
+            disableDownloadOfExternalAnnotations(project);
             TrustedProjects.setTrusted(project, true);
             ApplicationManager.getApplication().executeOnPooledThread(() -> {
                 StudioRequest lastRequest = listenForSyncRequests(project);
@@ -46,7 +49,19 @@ public class GradleProfilerProjectManagerListener implements ProjectManagerListe
         }
     }
 
-    private StudioRequest listenForSyncRequests(@NotNull com.intellij.openapi.project.Project project) {
+    private void disableDownloadOfExternalAnnotations(Project project) {
+        GradleSettings gradleSettings = GradleSettings.getInstance(project);
+        gradleSettings.getLinkedProjectsSettings()
+            .forEach(settings -> settings.setResolveExternalAnnotations(false));
+        gradleSettings.subscribe(new ExternalSystemSettingsListenerAdapter<>() {
+            @Override
+            public void onProjectsLinked(@NotNull Collection<GradleProjectSettings> linkedProjectsSettings) {
+                linkedProjectsSettings.forEach(settings -> settings.setResolveExternalAnnotations(false));
+            }
+        });
+    }
+
+    private StudioRequest listenForSyncRequests(@NotNull Project project) {
         int port = Integer.getInteger(PROFILER_PORT_PROPERTY);
         try (Client client = new Client(port)) {
             return new GradleProfilerClient(client).listenForSyncRequests(project);
