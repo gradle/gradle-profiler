830c6d2fc002e0305a69c8a25eb629566135becc
Anže Sodja <asodja@gradle.com>
2024-01-26 11:05:04 +0100

    Reformat DefaultGradleSettingsListener

--- subprojects/studio-plugin/src/main/java/org/gradle/profiler/studio/plugin/DefaultGradleSettingsListener.java
+++ subprojects/studio-plugin/src/main/java/org/gradle/profiler/studio/plugin/DefaultGradleSettingsListener.java
@@ -1,7 +1,5 @@
 package org.gradle.profiler.studio.plugin;
 
-import java.util.Collection;
-import java.util.Set;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import org.jetbrains.plugins.gradle.settings.DistributionType;
@@ -9,19 +7,54 @@ import org.jetbrains.plugins.gradle.settings.GradleProjectSettings;
 import org.jetbrains.plugins.gradle.settings.GradleSettingsListener;
 import org.jetbrains.plugins.gradle.settings.TestRunner;
 
+import java.util.Collection;
+import java.util.Set;
+
 /**
  * A default implementation of GradleSettingsListener that does nothing.
  */
 public class DefaultGradleSettingsListener implements GradleSettingsListener {
-    @Override public void onProjectsLinked(@NotNull Collection<GradleProjectSettings> settings) { }
-    @Override public void onGradleHomeChange(@Nullable String oldPath, @Nullable String newPath, @NotNull String linkedProjectPath) {  }
-    @Override public void onGradleDistributionTypeChange(DistributionType currentValue, @NotNull String linkedProjectPath) { }
-    @Override public void onServiceDirectoryPathChange(@Nullable String oldPath, @Nullable String newPath) { }
-    @Override public void onGradleVmOptionsChange(@Nullable String oldOptions, @Nullable String newOptions) { }
-    @Override public void onBuildDelegationChange(boolean delegatedBuild, @NotNull String linkedProjectPath) {  }
-    @Override public void onTestRunnerChange(@NotNull TestRunner currentTestRunner, @NotNull String linkedProjectPath) { }
-    @Override public void onProjectRenamed(@NotNull String oldName, @NotNull String newName) { }
-    @Override public void onProjectsUnlinked(@NotNull Set<String> linkedProjectPaths) {  }
-    @Override public void onBulkChangeStart() { }
-    @Override public void onBulkChangeEnd() { }
+    @Override
+    public void onProjectsLinked(@NotNull Collection<GradleProjectSettings> settings) {
+    }
+
+    @Override
+    public void onGradleHomeChange(@Nullable String oldPath, @Nullable String newPath, @NotNull String linkedProjectPath) {
+    }
+
+    @Override
+    public void onGradleDistributionTypeChange(DistributionType currentValue, @NotNull String linkedProjectPath) {
+    }
+
+    @Override
+    public void onServiceDirectoryPathChange(@Nullable String oldPath, @Nullable String newPath) {
+    }
+
+    @Override
+    public void onGradleVmOptionsChange(@Nullable String oldOptions, @Nullable String newOptions) {
+    }
+
+    @Override
+    public void onBuildDelegationChange(boolean delegatedBuild, @NotNull String linkedProjectPath) {
+    }
+
+    @Override
+    public void onTestRunnerChange(@NotNull TestRunner currentTestRunner, @NotNull String linkedProjectPath) {
+    }
+
+    @Override
+    public void onProjectRenamed(@NotNull String oldName, @NotNull String newName) {
+    }
+
+    @Override
+    public void onProjectsUnlinked(@NotNull Set<String> linkedProjectPaths) {
+    }
+
+    @Override
+    public void onBulkChangeStart() {
+    }
+
+    @Override
+    public void onBulkChangeEnd() {
+    }
 }
