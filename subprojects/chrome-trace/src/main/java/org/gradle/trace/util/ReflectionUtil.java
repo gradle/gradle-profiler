8fc2d63c4941191aaa0ecf1040cff819b9868b5b
commandini <commandini@users.noreply.github.com>
2025-08-20 19:22:37 +0200

    enhancements on ReflectionUtil#invokerGetter()
    Signed-off-by: commandini <commandini@users.noreply.github.com>

--- subprojects/chrome-trace/src/main/java/org/gradle/trace/util/ReflectionUtil.java
+++ subprojects/chrome-trace/src/main/java/org/gradle/trace/util/ReflectionUtil.java
@@ -4,13 +4,22 @@ import java.lang.reflect.Method;
 
 public interface ReflectionUtil {
 
-    static Object invokerGetter(Object object, String method) {
+    static Object invokerGetter(Object object, String methodName) {
+        boolean originalAccessibility;
+        Method method;
         try {
-            Method getterMethod = object.getClass().getMethod(method);
-            getterMethod.setAccessible(true);
-            return getterMethod.invoke(object);
+            method = object.getClass().getDeclaredMethod(methodName);
+            originalAccessibility = method.isAccessible();
+            method.setAccessible(true);
         } catch (Exception e) {
             throw new RuntimeException(e);
+        }
+        try {
+            return method.invoke(object);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        } finally {
+            method.setAccessible(originalAccessibility);
         }
     }
 }
