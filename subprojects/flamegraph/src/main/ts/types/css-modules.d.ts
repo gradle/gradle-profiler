0a9fb6f4ef9c1aa7347baf4546e947c92bdcc617
Justin Van Dort <jvandort@gradle.com>
2025-10-13 15:40:58 -0400

    Add simple flex layout containers

--- subprojects/flamegraph/src/main/ts/types/css-modules.d.ts
+++ subprojects/flamegraph/src/main/ts/types/css-modules.d.ts
@@ -1,1 +1,8 @@
 /// <reference types="vite/client" />
+
+declare module "*.module.css" {
+    const classes: { readonly [key: string]: string }
+    export default classes
+}
+
+declare module "*.css"
