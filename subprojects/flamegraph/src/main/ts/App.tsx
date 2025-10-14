8fb718b0ae28d47f331b8853ffe3d209fe7f1ec3
Justin Van Dort <jvandort@gradle.com>
2025-10-14 00:59:21 -0400

    Accept compressed base64 encoded data
    To embed this into gradle-profile, we will need to accept embedded -stacks files.
    This is because when opening an html file from a file:/// url, CORS does not allow it
    to access other files. So, we must embed the data in the file.
    
    To do this most efficiently, we compress the data, then we encode it in base64 to stick with printable characters and since base64 is well supported.
    Then, we encode our demo data in this format, and subsequently decode the encoded demo data, proving the encoding system works.

--- subprojects/flamegraph/src/main/ts/App.tsx
+++ subprojects/flamegraph/src/main/ts/App.tsx
@@ -14,6 +14,8 @@ import {
 import DataWorker from "./worker?worker&inline"
 import useWorkerPool from "./useWorkerPool.ts"
 import { Grow, Row, Stack } from "./containers.tsx"
+import { ENCODED_DEMO_STACKS } from "./demo.ts"
+import { decodeAndDecompressData, toStream } from "./encoding.ts"
 
 /**
  * Nodes with pixel width below this threshold will not be rendered.
@@ -542,15 +544,6 @@ interface WorkerState {
     graph?: GraphState
 }
 
-const stringToStream = (str: string): ReadableStream<Uint8Array> => {
-    return new ReadableStream({
-        start(controller) {
-            controller.enqueue(new TextEncoder().encode(str))
-            controller.close()
-        },
-    })
-}
-
 const App = (): React.JSX.Element => {
     const runJob = useWorkerPool<string, WorkerParams, WorkerResponse>(
         DataWorker,
@@ -626,51 +619,7 @@ const App = (): React.JSX.Element => {
 
     // Load the demo stacks
     useEffect(() => {
-        const stream = stringToStream(`alpha;component_one;foo;auth 10
-alpha;component_one;foo;process;db_read;connection_pool;check_idle_connections;check_feature_flag 65
-alpha;component_one;foo;process;db_read 150
-alpha;component_one;foo;process;transform;serialization_helper;object_mapper;write_buffer 20
-alpha;component_one;foo;logging 5
-alpha;component_two;foo;auth 12
-alpha;component_two;foo;process;db_read 40
-alpha;component_two;foo;process;transform;serialization_helper;object_mapper;instrumentation.increment_counter 45
-alpha;component_two;foo;process;transform 130
-alpha;component_two;foo;validate 50
-alpha;component_two;foo;logging 5
-asset_pipeline;process_image;decode_png 80
-asset_pipeline;process_image;resize 120
-asset_pipeline;process_image;compress_jpeg 150
-beta;foo;auth 5
-beta;foo;process;db_read;query_planner;optimizer;gc.safepoint 25
-beta;foo;process;db_read 10
-beta;foo;logging 3
-gamma;middleware;foo;auth 9
-gamma;middleware;foo;process;db_read 60
-gamma;middleware;foo;process;transform;serialization_helper;object_mapper;check_feature_flag 35
-gamma;middleware;foo;process;transform 55
-gamma;middleware;foo;logging 4
-alpha;component_one;foo;auth 14
-alpha;component_one;foo;process;db_read 135
-alpha;component_one;foo;process;transform;serialization_helper;object_mapper;instrumentation.increment_counter 30
-alpha;component_one;foo;process;transform 35
-alpha;component_one;foo;logging 6
-init_subsystem;load_config;parse_yaml 90
-init_subsystem;connect_downstream_api 110
-delta;service;foo;auth 20
-delta;service;foo;process;db_read 90
-delta;service;foo;process;transform 80
-delta;service;foo;validate 45
-delta;service;foo;logging 8
-beta;foo;auth 7
-beta;foo;process;db_read;connection_pool;check_idle_connections;check_feature_flag 15
-beta;foo;process;db_read 15
-beta;foo;logging 4
-gamma;middleware;foo;auth 11
-gamma;middleware;foo;process;db_read 55
-gamma;middleware;foo;process;transform 65
-gamma;middleware;foo;process;db_read;query_planner;optimizer;gc.safepoint 45
-gamma;middleware;foo;logging 5
-`)
+        const stream = toStream(decodeAndDecompressData(ENCODED_DEMO_STACKS))
         submitJob(
             "demo",
             {
