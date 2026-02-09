27186abc61dd31435a53ea042dba98978e874871
Alex Semin <asemin@gradle.com>
2026-02-09 14:24:24 +0100

    Upgrade to Async Profiler 4.3
    https://github.com/async-profiler/async-profiler/releases/tag/v4.3

--- src/main/java/org/gradle/profiler/asyncprofiler/AsyncProfilerCompatibility.java
+++ src/main/java/org/gradle/profiler/asyncprofiler/AsyncProfilerCompatibility.java
@@ -3,7 +3,7 @@ package org.gradle.profiler.asyncprofiler;
 public class AsyncProfilerCompatibility {
 
     // https://github.com/async-profiler/async-profiler/releases/
-    public static final AsyncProfilerVersion AUTO_DOWNLOAD_VERSION = new AsyncProfilerVersion("4.2");
+    public static final AsyncProfilerVersion AUTO_DOWNLOAD_VERSION = new AsyncProfilerVersion("4.3");
 
     public static final AsyncProfilerVersion MINIMUM_SUPPORTED_VERSION = new AsyncProfilerVersion("3.0");
 
