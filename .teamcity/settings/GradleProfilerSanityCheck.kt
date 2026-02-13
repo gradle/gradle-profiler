f944862d1f2632ce77a33e0464f15c8d09d01ed8
Alex Semin <asemin@gradle.com>
2026-02-13 21:25:26 +0100

    Add SanityCheck CI job

---
+++ .teamcity/settings/GradleProfilerSanityCheck.kt
@@ -0,0 +1,40 @@
+import jetbrains.buildServer.configs.kotlin.BuildType
+import jetbrains.buildServer.configs.kotlin.DslContext
+import jetbrains.buildServer.configs.kotlin.buildFeatures.commitStatusPublisher
+import jetbrains.buildServer.configs.kotlin.buildSteps.gradle
+
+object GradleProfilerSanityCheck : BuildType({
+    name = "Sanity Check"
+
+    gradleProfilerVcs()
+
+    val os = Os.linux
+    val arch = Arch.AMD64
+
+    params {
+        javaHome(os, arch, JavaVersion.OPENJDK_17)
+    }
+
+    steps {
+        gradle {
+            tasks = "clean sanityCheck"
+            buildFile = ""
+            gradleParams = "-s --build-cache ${toolchainConfiguration(os, arch)}"
+            param("org.jfrog.artifactory.selectedDeployableServer.defaultModuleVersionConfiguration", "GLOBAL")
+        }
+    }
+
+    features {
+        commitStatusPublisher {
+            vcsRootExtId = "${DslContext.settingsRoot.id}"
+            publisher = github {
+                githubUrl = "https://api.github.com"
+                authType = personalToken {
+                    token = "%github.bot-teamcity.token%"
+                }
+            }
+        }
+    }
+
+    agentRequirement(os, arch)
+})
