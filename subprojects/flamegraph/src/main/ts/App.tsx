e6be7091624ac3e1e2ad7c0bdd16105bd5c7804f
Justin Van Dort <jvandort@gradle.com>
2026-03-10 11:46:55 -0400

    Improve coloring

--- subprojects/flamegraph/src/main/ts/App.tsx
+++ subprojects/flamegraph/src/main/ts/App.tsx
@@ -42,15 +42,53 @@ const generateColorFor = (hue: number): string => {
     return `hsl(${hue}, 100%, ${lightness}%)`
 }
 
+const hashString = (str: string, distribution: number): number => {
+    let hash = 0
+    for (let i = 0; i < str.length; i++) {
+        hash = (hash << 5) - hash + str.charCodeAt(i)
+        hash |= 0
+    }
+    return (Math.abs(hash) % distribution) / distribution
+}
+
+const getParts = (name: string): string[] => {
+    // Strip method call and line number if present
+    let cleanName = name
+    const parenIdx = name.indexOf("(")
+    if (parenIdx !== -1) {
+        cleanName = name.substring(0, parenIdx)
+    } else if (name.endsWith("_[j]")) {
+        cleanName = name.substring(0, name.length - 4)
+    }
+
+    return cleanName
+        .split(/[./:]/)
+        .map((x) =>
+            x.indexOf("$") !== -1 ? x.substring(0, x.indexOf("$")) : x,
+        )
+        .filter((p) => p.length > 0)
+}
+
 /**
  * Given a string, deterministically generate a color.
  */
 const colorFor = (str: string, colorContext: ColorContextType): string => {
-    const lastSlash = str.lastIndexOf("/")
-    if (lastSlash !== -1) {
-        return generateColorFor(colorContext.colorCenter)
+    const { center, width, amount, distribution } = colorContext
+    const parts = getParts(str)
+
+    let hueAccumulator = 0
+    let currentDivisor = 1
+    for (let i = 0; i < parts.length; i++) {
+        // Shift hash from [0, 1] to [-0.5, 0.5] to center it around the 'center' hue.
+        hueAccumulator +=
+            (hashString(parts[i]!, distribution) - 0.5) *
+            (width / currentDivisor)
+        currentDivisor *= amount
     }
-    return generateColorFor(colorContext.colorCenter)
+
+    // Ensure the hue is always positive before modulo.
+    const hue = (center + hueAccumulator + 3600) % 360
+    return generateColorFor(hue)
 }
 
 const simpleName = (fullName: string): string => {
@@ -195,11 +233,17 @@ const NodeDetails = forwardRef<HTMLSpanElement, {}>((_, ref) => {
 })
 
 interface ColorContextType {
-    colorCenter: number
+    center: number
+    width: number
+    amount: number
+    distribution: number
 }
 
 const ColorContext = createContext<ColorContextType>({
-    colorCenter: 50,
+    center: 0,
+    width: 60,
+    amount: 1.5,
+    distribution: 1000,
 })
 
 const Slider: React.FC<{
@@ -217,7 +261,7 @@ const Slider: React.FC<{
         <input
             style={{ flexGrow: 1 }}
             type="range"
-            min={min}
+            min={0}
             max={resolution}
             value={scaledValue}
             onChange={(e) => {
@@ -376,7 +420,10 @@ const Flamegraph: React.FC<{
         handleMouseLeave()
     }, [graph])
 
-    const [colorCenter, setColorCenter] = useState(80)
+    const [colorCenter, setColorCenter] = useState(98)
+    const [colorWidth, setColorWidth] = useState(100)
+    const [colorAmount, setColorAmount] = useState(1.67)
+    const [colorDistribution, setColorDistribution] = useState(1199)
 
     return (
         <>
@@ -411,6 +458,33 @@ const Flamegraph: React.FC<{
                             onChange={setColorCenter}
                         />
                     </Row>
+                    <Row>
+                        Width ({Math.round(colorWidth)})
+                        <Slider
+                            min={0}
+                            max={360}
+                            value={colorWidth}
+                            onChange={setColorWidth}
+                        />
+                    </Row>
+                    <Row>
+                        Amount ({colorAmount.toFixed(2)})
+                        <Slider
+                            min={1}
+                            max={3}
+                            value={colorAmount}
+                            onChange={setColorAmount}
+                        />
+                    </Row>
+                    <Row>
+                        Distribution ({Math.round(colorDistribution)})
+                        <Slider
+                            min={1}
+                            max={5000}
+                            value={colorDistribution}
+                            onChange={setColorDistribution}
+                        />
+                    </Row>
                     <button
                         onClick={() => showMergedSubgraph(rootNode)}
                         disabled={rootNode === 0}
@@ -423,7 +497,14 @@ const Flamegraph: React.FC<{
                 </Stack>
             </Row>
 
-            <ColorContext.Provider value={{ colorCenter }}>
+            <ColorContext.Provider
+                value={{
+                    center: colorCenter,
+                    width: colorWidth,
+                    amount: colorAmount,
+                    distribution: colorDistribution,
+                }}
+            >
                 <Stack tall>
                     <div
                         style={{
